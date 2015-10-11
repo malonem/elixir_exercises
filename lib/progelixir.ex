fc542d802a20dd3696fc700d95f36a588be1eaec
Mark Malone <mark.malone@orbitz.com>
2015-10-11 14:12:04 -0500

    up to chp 7

--- lib/progelixir.ex
+++ lib/progelixir.ex
@@ -6,4 +6,17 @@ defmodule Progelixir do
   def fizzbuzz(x) do
     fizzword(rem(x,3), rem(x,5), x)
   end
+
+  def gcd(x, 0), do: x
+  def gcd(x, y), do: gcd(y, rem(x, y))
+
+  def reduce(low, high), do: div(high-low, 2)
+  def guess(number, low..high) when high > number do
+    guess(number, low..(reduce(low,high) + low))
+  end
+  def guess(number, low..high) when high < number do
+    guess(number, high..(reduce(low, high) + high + 1))
+  end
+  def guess(number, low..high) when high == number, do: number
+
 end
--- test/progelixir_test.exs
+++ test/progelixir_test.exs
@@ -23,4 +23,24 @@ defmodule ProgelixirTest do
       &(Progelixir.fizzbuzz(&1))
     ) == [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :buzz, 11, :fizz, 13, 14, :fizzbuzz]
   end
+
+  test "parameterized functions" do
+    prefix = fn pre -> (fn text -> pre <> " " <> text end) end
+    mrs = prefix.("Mrs")
+    assert mrs.("Smith") == "Mrs Smith"
+    assert prefix.("Elixir").("Rocks") == "Elixir Rocks"
+  end
+
+  test "aliasing functions" do
+    map = &Enum.map/2
+    assert map.([1,2,3,4], &(&1 + 2)) == [3,4,5,6]
+  end
+
+  test "greatest common divisor" do
+    assert Progelixir.gcd(36,10) == 2
+  end
+
+  test "guess a number in range" do
+    assert Progelixir.guess(273, 1..1000) == 273
+  end
 end
